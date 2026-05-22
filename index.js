e4c0f3437c2f3eb86405dd78e58b986c3563695a
Okan Roma <oktrian.pratama@gmail.com>
2026-05-22 18:33:25 +0700

    fix timezone WIB

--- index.js
+++ index.js
@@ -36,7 +36,11 @@ if (bot) {
   });
 
   bot.command("ping", (ctx) => {
-    const now = new Date().toLocaleString("id-ID");
+    const now = new Date().toLocaleString("id-ID", {
+      timeZone: "Asia/Jakarta",
+      dateStyle: "full",
+      timeStyle: "medium"
+    });
 
     ctx.reply(
       `🏓 Pong!\nStatus bot: Aktif ✅\nWaktu server: ${now}\n`
